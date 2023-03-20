aab1505b89f6fa53022f120c5152161ed480d4bb
Shugaba Wuta <shugabawuta@gmail.com>
2023-03-20 15:57:16 +0100

    WIP

---
+++ controllers/review-controller.js
@@ -0,0 +1,22 @@
+const { Review } = require("../models")
+
+const createAReview = async (req, res) => {
+    const { orderID, productID } = req.body
+
+}
+const updateReview = async (req, res) => {
+
+}
+const deleteReview = async (req, res) => {
+
+}
+const getReviews = async (req, res) => {
+
+}
+const getReview = async (req, res) => {
+
+}
+
+
+
+module.exports = { createAReview, updateReview, deleteReview, getReview, getReviews }
