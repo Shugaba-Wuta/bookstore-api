605358323866bbc772088bbe90679af461ec5416
Shugaba Wuta <shugabawuta@gmail.com>
2023-03-30 00:55:32 +0100

    Github actions complete

--- scripts/make-cookies-secure.sh
+++ scripts/make-cookies-secure.sh
@@ -1,1 +1,1 @@
-ls && sed -i "s/secure: false/secure: true/g" ./controllers/auth.js && sed -i "s/secure: false/secure: true/g" 'middleware/auth middleware.js'
+sed -i "s/secure: false/secure: true/g" ./controllers/auth.js && sed -i "s/secure: false/secure: true/g" 'middleware/auth middleware.js'
